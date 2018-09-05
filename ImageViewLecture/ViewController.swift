06e36c23114f362e24a44b7f830bf3715e9e31b9
Bailey Carlson <bcarlson9067@jh-mba158-02.local>
2018-09-05 10:56:40 -0500

    Initial Commit

---
+++ ImageViewLecture/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ImageViewLecture
+//
+//  Created by Bailey Carlson on 9/5/18.
+//  Copyright © 2018 John Hersey High School. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
